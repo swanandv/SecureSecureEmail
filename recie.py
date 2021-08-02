b2054922df453390972d28990f35d72fda001a68
swanandv <swanand.vartak@gmail.com>
2021-08-02 02:20:55 +0100

    1

---
+++ recie.py
@@ -0,0 +1,91 @@
+# Python 3.8.0
+from email import message
+import smtplib
+import time
+import imaplib
+import email
+import traceback 
+from email.header import decode_header
+import webbrowser
+import os
+# -------------------------------------------------
+#
+# Utility to read email from Gmail Using Python
+#
+# ------------------------------------------------
+ORG_EMAIL = "@gmail.com" 
+username = "bob.ca2.paul" + ORG_EMAIL 
+password = "bobpassword" 
+
+
+# create an IMAP4 class with SSL 
+imap = imaplib.IMAP4_SSL("imap.gmail.com")
+# authenticate
+imap.login(username, password)
+
+status, messages = imap.select("INBOX")
+
+#total no of messages in inbox
+m = int(messages[0]) 
+
+
+
+print(imap.noop())
+
+
+
+
+for i in range(m , m-4, -1):
+    # fetch the email message by ID
+    res, msg = imap.fetch(str(i), "(RFC822)")
+    for response in msg:
+        if isinstance(response, tuple):
+            # parse a bytes email into a message object
+            msg = email.message_from_bytes(response[1])
+            # decode the email subject
+            subject, encoding = decode_header(msg["Subject"])[0]
+            if isinstance(subject, bytes):
+                # if it's a bytes, decode to str
+                subject = subject.decode(encoding)
+            # decode email sender
+            From, encoding = decode_header(msg.get("From"))[0]
+            if isinstance(From, bytes):
+                From = From.decode(encoding)
+            print("Subject:", subject)
+            print("From:", From)
+            # if the email message is multipart
+            if msg.is_multipart():
+                # iterate over email parts
+                for part in msg.walk():
+                    # extract content type of email
+                    content_type = part.get_content_type()
+                    content_disposition = str(part.get("Content-Disposition"))
+                    try:
+                        # get the email body
+                        body = part.get_payload(decode=True).decode()
+                    except:
+                        pass
+                    if content_type == "text/plain" and "attachment" not in content_disposition:
+                        # print text/plain emails and skip attachments
+                        print(body)
+                    elif "attachment" in content_disposition:
+                        # download attachment
+                        filename = part.get_filename()
+                        if filename:
+                            folder_name = clean(subject)
+                            if not os.path.isdir(folder_name):
+                                # make a folder for this email (named after the subject)
+                                os.mkdir(folder_name)
+                            filepath = os.path.join(folder_name, filename)
+                            # download attachment and save it
+                            open(filepath, "wb").write(part.get_payload(decode=True))
+           
+            print("="*100)
+
+#for i in range(m, m-1, -1):
+#	if isinstance(response,tuple):
+		 # parse a bytes email into a message object
+		 # msg = email.message_from_bytes(response[1])
+
+
+		 
