7e855c3ab36e53a025829e558ab85c906f901729
swanandv <swanand.vartak@gmail.com>
2021-08-07 05:25:50 +0100

    Almost Final

--- MenuDriven.py
+++ MenuDriven.py
@@ -7,17 +7,19 @@ import imaplib
 from email.header import decode_header
 from email import message
 from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
 
 print("---Login to SecureSecureEmail---")
-#em = input("Enter EmailID:")
-#passwd =  getpass.getpass("Enter Password:")
+em = input("Enter EmailID:")
+passwd =  getpass.getpass("Enter Password:")
 
-em = "bob.ca2.paul@gmail.com"
-passwd = "bobpassword"
+#em = "bob.ca2.paul@gmail.com"
+#passwd = "bobpassword"
 #em = "alice.ca2.paul@gmail.com"
 #passwd = "alicepassword"
 
-print(em+" "+passwd)
+#print(em+" "+passwd)
 
 
 ## Provide email and password to api to create session
@@ -82,15 +84,15 @@ if choice=='3':
 
 	if choice1 == '1':
 ### logic to request key =>
-		recieEmail = input("Enter Receiver's Emailid:")
+		rec_email = input("Enter Receiver's Emailid:")
 		message = MIMEMultipart()
 		message['From'] = em
-		message['To'] = recieEmail
+		message['To'] = rec_email
 		message['Subject'] = 'SEND PUB KEY'
 		msg = open("Keys/public.pem").read()
 		message.attach(MIMEText(msg, 'plain'))
 		text = message.as_string()
-		session.sendmail(em, recieEmail, text)
+		session.sendmail(em, rec_email, text)
 		print("Key request sent!")
 		print("Key will be recieved whenever requested party opens the request. Keep eyes on inbox.")
 		
@@ -184,7 +186,62 @@ if choice=='3':
 
 
 #### Choice 2 Inbox
-#.if choice == '2':
+if choice == '2':
+	M = imaplib.IMAP4_SSL("imap.gmail.com")
+	M.login(em, passwd)
+
+	M.select("INBOX")
+	typ, data = M.search(None, 'ALL')
+	print("="*100)
+	for num in data[0].split():
+		print("SNo. "+num.decode())
+		typ, data = M.fetch(num, '(RFC822)')
+		msg = email.message_from_bytes(data[0][1])
+		#print(msg)
+		subject, encoding = decode_header(msg["Subject"])[0]
+		From, encoding = decode_header(msg.get("From"))[0]
+		print("Subect: "+subject)
+		print("From: "+From)
+		print("-"*100)
+	print("="*100)
+
+	selectEmail = input("Enter Email Number to view:")
+	typ, data = M.fetch(selectEmail, '(RFC822)')
+	msg = email.message_from_bytes(data[0][1])
+	subject, encoding = decode_header(msg["Subject"])[0]
+	print(subject)
+	From, encoding = decode_header(msg.get("From"))[0]
+	print(From)
+
+	# iterate over email parts
+	for part in msg.walk():
+		content_type = part.get_content_type()
+		content_disposition = str(part.get("Content-Disposition"))
+		try:
+			body = part.get_payload(decode=True)
+		except:
+			pass
+		if content_type == "text/plain" and "attachment" not in content_disposition:
+			#print(body)
+			private_key = RSA.import_key(open("Keys/private.pem").read())
+			####Slice the byte stream
+			enc_session_key = body[:private_key.size_in_bytes()]
+			nonce = body[private_key.size_in_bytes():private_key.size_in_bytes()+16]
+			tag = body[private_key.size_in_bytes()+16:private_key.size_in_bytes()+16+16]
+			ciphertext = body[private_key.size_in_bytes()+16+16:]	
+			#### RSA decrypt
+			cipher_rsa = PKCS1_OAEP.new(private_key)
+			session_key = cipher_rsa.decrypt(enc_session_key)
+			#### AES decrypt	
+			cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+			data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+			print("\nDecrypted Message:")
+			print(data.decode("utf-8"))
+
+
+
+
+	
 
 
 
@@ -192,20 +249,45 @@ if choice=='3':
 
 ### Choice 3 Send mail
 if choice == '1':
-	recieEmail = input("Enter Email:")
+	rec_email = input("Enter Email:")
+	## Check if keys are there for the entered emailid if yes then read recipients pub key
+	try:
+		recipient_key = RSA.import_key(open("Contacts/"+rec_email+".pem").read())
+	except FileNotFoundError:
+		print("Entered Email is not in your Contacts. Please exchange keys first.")
+		exit()
+
 	subject = input("Enter Subject:")
-	msg = input("Enter Message:")
+	msg = input("Enter Message:").encode("utf-8")
 
 	#### Craft the email
 	message = MIMEMultipart()
 	message['From'] = em
-	message['To'] = recieEmail
+	message['To'] = rec_email
 	message['Subject'] = subject
-	message.attach(MIMEText(msg, 'plain'))
+
+	########### ENCRYPTION #############################
+	session_key = get_random_bytes(16)
+	cipher_rsa = PKCS1_OAEP.new(recipient_key)
+	enc_session_key = cipher_rsa.encrypt(session_key)
+
+	cipher_aes = AES.new(session_key, AES.MODE_EAX)
+	ciphertext, tag = cipher_aes.encrypt_and_digest(msg)
+
+	body = enc_session_key + cipher_aes.nonce + tag + ciphertext
+
+
+	
+
+
+	message.attach(MIMEText(body, 'plain','utf-8'))
 
 	### Send Email
 	text = message.as_string()
-	session.sendmail(em,recieEmail, text)
+
+	print("\n\n"+text)
+	session.sendmail(em,rec_email, text)
+	print("Encrypted Email Sent!")
 
 
 
--- filenametest.py
+++
@@ -1,3 +0,0 @@
-f = open("Personal/bob.ca2.paul@gmail.com.pem","wb")
-
-f.write(b"Hi")
